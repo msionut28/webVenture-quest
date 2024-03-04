69bc41f844ce7add2868d1328fa37cff1ec5c59c
msionut28 <moloceaionutt@gmail.com>
2024-03-04 11:19:47 +0200

    initial layout

---
+++ components/Button/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({text}) => {
+    return(
+        <button className="bg-slate-700">{text}</button>
+    )
+}
+
+export default Button
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import Button from '../Button/Button'
+
+const Navbar = () => {
+    return(
+        <div>
+            <Button  text={'test'}/>
+        </div>
+    )
+}
+export default Navbar
