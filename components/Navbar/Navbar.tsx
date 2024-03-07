715c66f813a20ad167f5a988f9094d16360afc2c
msionut28 <moloceaionutt@gmail.com>
2024-03-07 16:08:09 +0200

    navbar styling + fonts

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,23 +2,25 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-2/6 bg-slate-500 gap-2 flex">
-      <Link href="/">
-        <button>HOME</button>
-      </Link>
-      <Link href="/games">
-        <button>GAMES</button>
-      </Link>
-      <Link href="/learn">
-        <button>LEARN</button>
-      </Link>
+    <div className="w-full h-2/6 flex">
+      <div className="ml-6 gap-2 flex">
+        <Link href="/">
+          <button className="roboto">HOME</button>
+        </Link>
+        <Link href="/games">
+          <button className="roboto">GAMES</button>
+        </Link>
+        <Link href="/learn">
+          <button className="roboto">LEARN</button>
+        </Link>
+      </div>
       <div className="ml-auto mr-4 flex gap-2">
-      <Link href="/profile">
-        <button>PROFILE</button>
-      </Link>
-      <Link href="/login">
-        <button>LOGIN</button>
-      </Link>
+        <Link href="/profile">
+          <button className="roboto">PROFILE</button>
+        </Link>
+        <Link href="/login">
+          <button className="roboto">LOGIN</button>
+        </Link>
       </div>
     </div>
   );
