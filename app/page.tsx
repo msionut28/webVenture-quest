a3a045abc07a8bdd5aef3485ed5ae61350272045
msionut28 <moloceaionutt@gmail.com>
2024-03-07 17:19:28 +0200

    svgs added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex justify-center items-center">
-        <h1 className="text-4xl madimi">This is WebVenture</h1>
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl madimi">This is WebVenture</h1>
+      <div className="flex">
+        <Image
+          src={"/captain_donatello/hand-wave.svg"}
+          alt="donatello waving"
+          width={300}
+          height={300}
+        />
+        <Image
+          src={"/captain_donatello/standing.svg"}
+          alt="donatello standing"
+          width={300}
+          height={300}
+        />
+        <Image
+          src={"/captain_donatello/walking.svg"}
+          alt="donatello walking"
+          width={500}
+          height={500}
+        />
+        <Image
+          src={"/captain_donatello/sitting.svg"}
+          alt="donatello sitting"
+          width={500}
+          height={500}
+        />
+      </div>
     </main>
   );
 }
