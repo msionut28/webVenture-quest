c1005ca9603478d25f7d1b7a60e481bb15acedf1
msionut28 <moloceaionutt@gmail.com>
2024-03-21 11:27:19 +0200

    shadcn + register link

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,12 @@
-import type { Metadata } from "next"
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Register',
-  description: "Create an account"
-}
+  title: "Register",
+  description: "Create an account",
+};
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  return <div>Register</div>;
+};
 
-export default Register
+export default Register;
