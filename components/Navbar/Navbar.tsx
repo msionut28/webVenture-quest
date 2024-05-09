fe08f7e36ca44979f7c36a9a7dd8dd3f1db561e7
msionut28 <moloceaionutt@gmail.com>
2024-05-10 00:16:28 +0300

    navbar update + lottie

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { Logout } from "@/components/index";
+import './style.css'
 
 // Navbar component for navigation bar
 const Navbar = () => {
@@ -11,52 +12,55 @@ const Navbar = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="w-full h-2/6 flex">
+    // <nav className="navbar w-full h-2/6 flex backdrop-blur-sm sticky top-0">
+    <nav className="navbar backdrop-blur-sm sticky top-0">
       {/* Left section of the navbar */}
-      <div className="ml-6 gap-2 flex">
+      <div className="link-btns">
+      {/* <div className="list gap-2 text-center"> */}
         {/* Home link */}
         <Link href="/">
-          <button>HOME</button>
+          <button className="link-btn">HOME</button>
         </Link>
         {/* Games link */}
-        <Link href="/games">
-          <button>GAMES</button>
+        <Link  href="/games">
+          <button className="link-btn">GAMES</button>
         </Link>
         {/* Learn link */}
         <Link href="/learn">
-          <button>LEARN</button>
+          <button className="link-btn">LEARN</button>
         </Link>
-      </div>
+      {/* </div> */}
       {/* Right section of the navbar */}
-      <div className="ml-auto mr-4 flex gap-2">
+      {/* <div className="ml-auto mr-4 flex gap-2"> */}
         {/* Conditional rendering based on session existence */}
         {session ? (
           // Display Chat link if session exists
           <Link href="/chat">
-            <button>CHAT</button>
+            <button className="link-btn">CHAT</button>
           </Link>
         ) : (
           // Display Login link if session doesn't exist
           <Link href="/login">
-            <button>LOGIN</button>
+            <button className="link-btn">LOGIN</button>
           </Link>
         )}
         {/* Conditional rendering based on session existence */}
         {session ? (
           // Display Profile link if session exists
           <Link href="/profile">
-            <button>PROFILE</button>
+            <button className="link-btn">PROFILE</button>
           </Link>
         ) : (
           // Display Register link if session doesn't exist
           <Link href="/register">
-            <button>REGISTER</button>
+            <button className="link-btn">REGISTER</button>
           </Link>
         )}
         {/* Render Logout component if session exists */}
-        {session && <Logout />}
+        {session && <div className="link-btn"><Logout /></div>}
+      {/* </div> */}
       </div>
-    </div>
+    </nav>
   );
 };
 
