674ce242fd8d918f3ad63eb5616005f51d600bae
msionut28 <moloceaionutt@gmail.com>
2024-08-22 17:15:27 +0300

    registration done, + colour tweaks

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,34 +1,76 @@
 import { NextRequest, NextResponse } from "next/server";
 import credentialsAuthorizeUser from "@/db/actions/user/credentialsAuthorizeUser";
+import jwt from "jsonwebtoken";
+import { serialize } from "cookie";
 
-export async function POST (req: NextRequest) {
-    if (req.method === 'POST') {
-        try {
-            const info = await req.json()
+export async function POST(req: NextRequest) {
+  if (req.method === "POST") {
+    try {
+      const info = await req.json();
 
-            //Calling user login logic
-            const user = await credentialsAuthorizeUser(info);
-            return NextResponse.json(
-                {
-                  message: "User logged in successfully",
-                  user
-                },
-                {
-                  status: 201,
-                }
-              );
-        } catch (error) {
-            console.error("Error logging in user:", error);
-      
-            // Handling errors appropriately
-            return NextResponse.json(
-              {
-                message: "Error logging in user",
-              },
-              {
-                status: 400,
-              }
-            );
+      //Calling user login logic
+      const user = await credentialsAuthorizeUser(info);
+
+      if (!user) {
+        return NextResponse.json(
+          {
+            message: "Invalid Credentials",
+          },
+          {
+            status: 401,
           }
+        );
+      }
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        throw new Error("JWT_SECRET environment variable is not defined");
+      }
+
+      const token = jwt.sign({ userId: user.id }, secret as jwt.Secret, {
+        expiresIn: "1h",
+      });
+
+      const cookie = serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 60 * 60,
+        sameSite: "strict",
+        path: "/",
+      });
+
+      const response = NextResponse.json(
+        {
+          message: "User logged in successfully",
+          user,
+          cookie
+        },
+        {
+          status: 201,
+        }
+      );
+      response.headers.set("Set-Cookie", cookie);
+      return response;
+    } catch (error) {
+      console.error("Error logging in user:", error);
+
+      // Handling errors appropriately
+      return NextResponse.json(
+        {
+          message: "Error logging in user",
+        },
+        {
+          status: 400,
+        }
+      );
     }
+  } else {
+    return NextResponse.json(
+      {
+        message: "Method not allowed",
+      },
+      {
+        status: 405,
+      }
+    );
+  }
 }
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,10 +1,3 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Games",
-  description: "Play our games in order to unlock new levels",
-};
-
 const Games = () => {
   return <div>Games</div>;
 };
